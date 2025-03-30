bf4f3b5b7f04def18fe9b6371ad4d4ea381a1817
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2025-03-30 12:14:22 +0900

    test

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -49,6 +49,7 @@ resource "aws_iam_role" "ecr-access-2" {
   })
 }
 
+// invalid comment
 resource "aws_iam_role_policy_attachment" "ecr_access_attachment" {
   role       = aws_iam_role.ecr_access.name
   policy_arn = aws_iam_policy.read_only_ecr_access.arn
