7e9ccaffc0adac0e20164f2cc7ed6866275d512a
Yuki Yamamura <126521201+yuki-yamamura@users.noreply.github.com>
2025-03-30 11:28:36 +0900

    test

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -49,6 +49,11 @@ resource "aws_iam_role" "ecr_access_2" {
   })
 }
 
+resource "aws_instance" "app" {
+  ami           = "ami-0ff8a91507fiii"
+  instance_type = "t1.micro" # invalid type!
+}
+
 resource "aws_iam_role_policy_attachment" "ecr_access_attachment" {
   role       = aws_iam_role.ecr_access.name
   policy_arn = aws_iam_policy.read_only_ecr_access.arn
